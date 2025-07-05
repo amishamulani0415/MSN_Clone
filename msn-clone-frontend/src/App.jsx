f1716744c1b3dd68bb6a3e502cb906cb41da0d61
Gokulrajan <143718403+ngrajan@users.noreply.github.com>
2025-07-05 23:39:40 +0530

    initial setup

---
+++ msn-clone-frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import SamplePage from "./pages/SamplePage";
+
+function App() {
+  return (
+    <>
+      <div>
+        <SamplePage />
+      </div>
+    </>
+  );
+}
+
+export default App;
---
+++ msn-clone-frontend/src/config/apiPath.js
@@ -0,0 +1,6 @@
+const apiPath = {
+  sampleEndpoint: "msnews",
+  // in the value field (inside the quotes) you should put the endpoint path like in backend "/sample" or "/users" etc.,
+};
+
+export default apiPath;
